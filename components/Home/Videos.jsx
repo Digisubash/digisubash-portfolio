661fc5ba14d13d574f302fe557f8cf3d6fb4437b
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-08-15 10:35:18 +0530

    Content update

--- components/Home/Videos.jsx
+++ components/Home/Videos.jsx
@@ -4,23 +4,45 @@ const Videos = () => {
   return (
     <section id="video" className="section-1 highlights image-center">
       <div className="container smaller">
-        <div className="row text-center intro">
+        <div className="row intro">
           <div className="col-12">
-            <span className="pre-title">Introduction Video</span>
-            <h2>
-              Advisory{" "}
-              <span className="featured">
-                <span>Performance</span>
-              </span>
-            </h2>
+            <span className="pre-title"> Seo </span>
+            <center>
+              <h2>
+                What and{" "}
+                <span className="featured">
+                  <span>Why</span>
+                </span>
+              </h2>
+            </center>
+
             <p className="text-max-800">
-              Financial executives need to review their strategies, operating
-              models and capabilities so that their areas can generate sound
-              information for better decision making.
+              Living in an Internet world, being digital has now become
+              essential in the field of marketing. Digital plays a prominent
+              role in all marketing strategies and SEO is the vital tool of
+              digital marketing.
+            </p>
+            <br />
+
+            <p className="text-max-800">
+              For a clear understanding of SEO, and for knowing why it's much
+              important in digital marketing, knowledge about the multiple
+              elements of SEO by which it's made of and how these elements work
+              are the key.
+            </p>
+            <br />
+            <p className="text-max-800">
+              SEO is very much essential in bringing more traffic to your
+              website, that is making your website more visible to your
+              prospects and converting them from prospects to your customers. It
+              also works as an important tool that makes awareness for your
+              brand and places you among the experts of your field by building
+              valuable and trustworthy relationships with your prospects and
+              customers.
             </p>
           </div>
         </div>
-        <div className="row text-center">
+        {/* <div className="row text-center">
           <div className="col-12 gallery">
             <a
               href="#"
@@ -40,7 +62,7 @@ const Videos = () => {
               />
             </a>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
